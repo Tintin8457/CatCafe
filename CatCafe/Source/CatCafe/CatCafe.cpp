30748fc140d8f7b40a3c6776b72c6356831c9952
frankjrosario <37050007+frankjrosario@users.noreply.github.com>
2021-11-16 01:11:27 -0500

    .

--- CatCafe/Source/CatCafe/CatCafe.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CatCafe.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CatCafe, "CatCafe" );
-
-DEFINE_LOG_CATEGORY(LogCatCafe)
- 
