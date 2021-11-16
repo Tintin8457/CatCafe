decddc84e263a44922a721f706aecb8508ebceac
frankjrosario <37050007+frankjrosario@users.noreply.github.com>
2021-11-16 01:20:54 -0500

    Revert "."
    This reverts commit 30748fc140d8f7b40a3c6776b72c6356831c9952.

---
+++ CatCafe/Source/CatCafe/CatCafe.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CatCafe.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CatCafe, "CatCafe" );
+
+DEFINE_LOG_CATEGORY(LogCatCafe)
+ 
